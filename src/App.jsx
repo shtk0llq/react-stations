58b2387f0c98d1c7dbd9cbf1e2543bc184f661f4
shtk0llq <shtk0llq@gmail.com>
2024-08-21 11:17:02 +0900

    station 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spitz-indian/Indian_Spitz.jpg');
+
   return (
     <>
       <header>
@@ -14,7 +17,7 @@ export const App = () => {
 
       <h2>犬の画像を表示するサイトです。</h2>
 
-      <img src='https://images.dog.ceo/breeds/spitz-indian/Indian_Spitz.jpg' alt='dog image' />
+      <img src={dogUrl} alt='dog image' />
     </>
   )
 }
