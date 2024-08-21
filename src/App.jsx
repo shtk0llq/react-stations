2872c8dad4e41c37e9d99e58f3ed947b83799db9
shtk0llq <shtk0llq@gmail.com>
2024-08-21 20:52:28 +0900

    station 5

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ export const App = () => {
       <h2>犬の画像を表示するサイトです。</h2>
 
       <img src={dogUrl} alt='dog image' />
+
+      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>
+        更新
+      </button>
     </>
   )
 }
